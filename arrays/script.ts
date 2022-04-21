fd151c1a2fd89fd28d28fd16770ea457f3009f1c
Couto28 <di.diogocouto@gmail.cmo>
2022-04-21 02:27:54 -0300

    terceiro commit

---
+++ arrays/script.ts
@@ -0,0 +1,21 @@
+// criando e especificando o tipos dos valores dentro do array no typescript.
+
+let arr: string[] = ["Diogo", "Couto", "Santos",]; // toda vez que eu criar um array de string utilizo : string[]
+
+let num: number[] = [1, 2, 3, 4, 5];  // toda vez que eu criar um array de numeors utilizo : number[]
+let numeros: Array<number> =[1, 2, 3, 4]; // outra forma de declarar array no Tsc ... serve para todos os tipos de dados.
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Para converter o código  - terminal , digite tsc nome do arquivo "script.ts" */
+// toda vez que for testar o código , devemos converter ele .
---
+++ tiposdedados/script.ts
@@ -0,0 +1,10 @@
+// Como adicionar o tipo da variavel no Tsc .
+
+// após o valor da varivael use " : tipo dela " string 
+
+let nome: string = "Diogo";  // sempre recomendavel que quando estiver tipando usar letras minusculaas no tsc
+
+let numero: number = 90 ;
+
+let programador: boolean = true ;
+
